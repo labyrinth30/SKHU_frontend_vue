9c7de37305723a5313581fb0510baac24a9bc835
younha00 <ayeon0@office.skhu.ac.kr>
2023-11-01 19:33:01 +0900

    11월 1일 강의

--- vue_app21/src/App.vue
+++ vue_app21/src/App.vue
@@ -1,36 +1,55 @@
 <template>
   <div id="app">
     <h1>MyText</h1>
-    <p>{{ receivedText }}</p>
-    <MyText title="MyText" color="#ffb" @text="showText" />
+    <select v-model="color" >
+      <option value="#ffe">yellow</option>
+      <option value="#eef">blue</option>
+      <option value="#efe">green</option>
+      <option value="#fee">red</option>
+      </select>
+<p />
+      <input type="text" v-model="message" />
+      <hr/>
+      <MyMessage :bgColor= "color" :text="message" />
+
   </div>
 </template>
-
+<!-- 평범한 애트리뷰트의 값은 문자열 취급함-->
+<!-- 앱의 color 속성값, 즉 js 코드-->
+<!-- 고로 v-bind를 붙여야한다-->
 <script>
-import MyText from './MyText.vue'
+import MyMessage from './MyMessage.vue'
 
 export default {
   name: "App",
-  data() {
+  data() { 
     return {
-      receivedText: ""
+      color: "#ffe",
+      message : 'Hello World'
     }
   },
-  methods: {
-    showText(value) {
-      this.receivedText = value;
-    }
-  },
-  components: { MyText }
+  
+  
+  components: { MyMessage },
 }
 </script>
 
 <style scoped>
 div#app {
-  padding: 0 30px 30px 30px;
+  padding: 30px;
   margin: 30px auto;
   max-width: 400px;
   border: 1px solid #ccc;
   box-shadow: 3px 3px 3px #aaa;
+}
+hr{
+  margin: 10px 0;
+}
+select{
+  padding: 4px;
+  width: 100px;
+}
+input{
+  padding: 4px;
 }
 </style>
---
+++ vue_app21/src/MyMessage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="MyMessage" :style="{backgroundColor: bgColor }">
+        {{ text }}
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "MyTemplate",
+    props: {
+      bgColor: String,
+      text: String,
+    },
+    
+    
+  }
+  </script>
+  
+  <style scoped> /* scoped는 이 컴포넌트에만 적용하도록 붙임 */
+    
+    div#MyMessage { padding: 20px; border: 1px solid gray;  font-size: 15pt; }
+  </style>
+  
+  
