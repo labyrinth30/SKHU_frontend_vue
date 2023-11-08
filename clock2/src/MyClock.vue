d6dabb1d0b2d2b45e7261a0a8e17ff38bfb7d5ae
younha00 <ayeon0@office.skhu.ac.kr>
2023-11-08 16:21:40 +0900

    11월 8일 강의

---
+++ clock2/src/MyClock.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>{{ time }}</div>
+  </template>
+  
+  <script>
+  import moment from "moment";
+  
+  export default {
+    name: "MyClock",
+    props: {
+      format: { type: String, default: "HH:mm:ss" }
+    },
+    data() {
+      return { time: null };
+    },
+    mounted() {
+        // 컴포넌트가 mount된 직후에 자동으로 호출되는 메서드
+        // mount == 화면에 보이게 되는 것
+        // 현재 시간을 time 변수에 담아 리턴함
+        // updated는 만들고 재렌더링한 것
+      let callback = () => this.time = moment().format(this.format)
+      callback();
+      this.timerId = setInterval(callback, 1000);
+    },
+    beforeUnmount() {
+      clearInterval(this.timerId);
+    }
+  }
+  </script>
+  
+  <style scoped>
+  div { display: inline-block; }
+  </style>
+  
+  
---
+++ vue_app3/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @는 /src 라는 뜻, 절대 경로
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
